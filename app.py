7161fe94f532b8e049c437df0972f1f01e1ae8c6
milonofff <alekseymilonov@gmail.com>
2022-05-16 21:25:13 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+'''
+имя валюты, цену на которую надо узнать, — base (основа, за какую валюту мы хотим купить);
+имя валюты, цену в которой надо узнать, — quote (назначенная цена, валюту которую мы хотим купить);
+количество переводимой валюты — amount (количество)
+'''
+
+import telebot
+from config import keys, TOKEN
+from extensions import APIException, MoneyConverter
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start','help'])
+def help(message: telebot.types.Message):
+    text = "Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты> " \
+           "<в какую валюту перевести>" \
+           "<количество переводимой валюты>" \
+           "<список всех доступных валют: /values"
+    bot.reply_to(message, text)
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = "Доступные валюты:"
+    for key in keys.keys(): # Из словаря keys достаем ключи и итерируем
+        text = '\n'.join((text,key, ))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text',])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ValueError('Слишком много или мало параметров!')
+
+        quote, base, amount = values
+        total_base = MoneyConverter.get_price(quote, base, amount)
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка пользователя\n{e}")
+    except Exception as e:
+        bot.reply_to(message, f"Не удалось обработать команду\n{e}")
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+bot.polling(non_stop=True)
---
+++ config.py
@@ -0,0 +1,8 @@
+TOKEN = '5159342904:AAFj0wBN2S0SdBPOmRsilb3JKus_20HAqFc'
+
+keys = {
+    'доллар':'USD',
+    'рубль':'RUB',
+    'евро':'EUR',
+    'юань':'CNY'
+}
